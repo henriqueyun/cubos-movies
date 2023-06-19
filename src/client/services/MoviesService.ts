dd7d0f4702879a04f60fbd3383fec08e71ca15b8
henriqueyun <henriqueyun@gmail.com>
2023-06-19 07:14:30 -0300

    feat: added README.md and removed hardcoded API_KEY (disabled key)

--- src/client/services/MoviesService.ts
+++ src/client/services/MoviesService.ts
@@ -3,11 +3,11 @@ import { AxiosStatic } from "axios"
 function MoviesService(http: AxiosStatic) {
     return {
         search: async (query = "2023", page = 1) => {
-            const { data } = await http.get(`/search/movie?query=${query}&page=${page}&api_key=dac67aca7688e3f7d6b2bc7850308633`)
+            const { data } = await http.get(`/search/movie?query=${query}&page=${page}&api_key=${import.meta.env.VITE_API_KEY}`)
             return data;
         },
         discover: async (page = 1) => {
-            const { data } = await http.get(`/discover/movie?page=${page}&api_key=dac67aca7688e3f7d6b2bc7850308633`)
+            const { data } = await http.get(`/discover/movie?page=${page}&api_key=${import.meta.env.VITE_API_KEY}`)
             return data;
         }
     }
